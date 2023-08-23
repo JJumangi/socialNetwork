ab587dad603aa6c2466f3312bbd118b9a9714220
Jon A <jon.jumangi@gmail.com>
2023-08-23 14:43:34 -0500

    fixing models/reactions

---
+++ models/Reaction.js
@@ -0,0 +1,31 @@
+const {Schema,Types} = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280
+    },
+    username: {
+      type: String,
+      require: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timeStamp => dateFormate(timeStamp)
+    }
+  },
+  {
+    toJSONL: {
+      getters: true
+    },
+    id: false
+  }
+);
+module.exports= reactionSchema;
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,1 +1,6 @@
 const router = require('express').Router();
+
+//users
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get()
